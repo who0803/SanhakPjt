854d4bed75d090a2b125e9233193954265391574
who0803 <65334125+who0803@users.noreply.github.com>
2021-04-14 14:02:20 +0900

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,32 +1,55 @@
 var selection = document.querySelectorAll('.seletion');
-var count = 0;  /* 누르기 전 상태 */
-var count2 = 0; /* 버튼 */
+var beforeCount = 0;  /* 누르기 전 상태.  선택전 카운트의 개수 */
+var afterCount = 0; /* 버튼. 선택후 카운트의 개수 */
+const btn = document.querySelector('#btn');
+let feeling = document.querySelector('#feeling');
+let feelingData = [];    // 감정 객체 데이터 배열
+
+fetch("https://zbzvef33kf.execute-api.ap-northeast-2.amazonaws.com/prod/sentences?userId=" + localStorage.getItem('userId') + "&historyId=" + localStorage.getItem('historyId'))
+  .then((response) => response.json())
+  .then((data) => {
+      feeling.innerHTML = data.body.emotion;
+      for (let i = 0; i < 4; i++) {
+        selection[i].innerHTML =  data.body.sentences[i][0].Content;
+        feelingData[i] = data.body.sentences[i][0];
+      }
+      
+      console.log(data);
+      console.log(data.body.sentences[0][0].Content);
+      //console.log('userId = ', localStorage.getItem('userId'))
+      //console.log('historyId = ', localStorage.getItem('historyId'))
+    })
 
 selection.forEach(function (e) {  // 이런식으로 포문 돌려서 핸들러 추가
     e.addEventListener('click', function(e) { // 인덱스로 접근
-        count = 0;
+        beforeCount = 0;
         for (var i = 0; i < selection.length; i++) {    /* 선택전 카운트의 개수 */
             if (selection[i].classList.contains('active')) {    /* 하나씩 다 검사 */
-                count++;
+                beforeCount++;
             }
         }
-        console.log("1: " + count);
-        if (count === 3 && !(e.currentTarget.classList.contains('active'))) return; /*active가 이미 3개있는데 눌린거 말고 다른 거 눌리면*/
+        console.log("1: " + beforeCount);
+        if (beforeCount === 3 && !(e.currentTarget.classList.contains('active'))) return; /*active가 이미 3개있는데 눌린거 말고 다른 거 눌리면*/
         else e.currentTarget.classList.toggle('active');
         
-        count2 = 0;
+        afterCount = 0;
         for (var i = 0; i < selection.length; i++) {    /* 선택후 카운트의 개수 */
             if (selection[i].classList.contains('active')) {    /* 하나씩 다 검사 */
-                count2++;
+                afterCount++;
             }
         }
     });
 });
 
 
-function newPage()  {
-    console.log("2: "+count);
-    if (count2 !== 0) {
+btn.addEventListener('click', function() {
+    //console.log("2: "+count);
+    if (afterCount !== 0) { // 하나라도 선택된 것이 있어야 가능
+        
         window.location.href = '2.html'
     }
-}
+});
+
+
+
+
